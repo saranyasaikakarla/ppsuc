3e9d8c81e93913760128430e25a52dc1e4fb1804
saranyasaikakarla <saranyasaikakarla@gmail.com>
2025-11-06 14:46:55 +0530

    Add files via upload

---
+++ add sub mil div mod.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{int a,b;
+printf("enter a value:");
+scanf("%d",&a);
+printf("enter b value:");
+scanf("%d",&b);
+printf("Addition(a+b)=%d",a+b);
+printf("subraction(a-b)=%d",a-b);
+printf("multiplicaion(a*b)=%d",a*b);
+printf("division(a/b)=%d",a/b);
+printf("modulus(a%b)=%d",a%b);
+return 0;
+}
